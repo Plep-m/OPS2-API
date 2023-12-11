f04d10da760ea8379bf6a1a97023fd43cc1ebeac
Plep-m <menut34@gmail.com>
2023-12-11 16:27:07 +0100

    tests

--- tests/test_user_routes.py
+++ tests/test_user_routes.py
@@ -9,5 +9,15 @@ def test_root():
     assert response.json() == ['Hello World !!!']
 
 def test_connect_wrongpass_ko():
-    response = client.post("/connect/", data={"user_login": "sully_natsuya", "password": "notthepassword"})
-    assert response.status_code == 200
+  response = client.post(
+    "/connect/",
+    headers={
+      "Content-Type": "application/json"
+    },
+    json={
+      "user_login": "sully_natsuya",
+      "password": "notthepassword"
+    }
+  )
+  assert response.status_code == 401
+  assert response.json() == {"detail": "Invalid credentials"}
