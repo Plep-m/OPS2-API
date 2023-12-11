a8cea99ab72f540cd3008fb44990b8f3b53da1f3
Plep-m <menut34@gmail.com>
2023-12-11 15:47:40 +0100

    tests

--- tests/test_user_routes.py
+++ tests/test_user_routes.py
@@ -7,3 +7,8 @@ def test_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == ['Hello World !!!']
+
+def test_verify_token_ko():
+    response = client.get("/verify-token/")
+    assert response.status_code == 401
+    assert response.json() == {'detail': 'Could not validate credentials'}
