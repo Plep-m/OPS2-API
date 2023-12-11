e61b35e87913f639750683e28fb15c5162544274
Plep-m <menut34@gmail.com>
2023-12-11 17:01:34 +0100

    tests

--- tests/test_user_routes.py
+++ tests/test_user_routes.py
@@ -9,7 +9,7 @@ def test_root():
     assert response.json() == ['Hello World !!!']
   
 def test_get_user_by_login_ok():
-  response = client.get("/users/sully_natsuya")
+  response = client.get("/user/sully_natsuya")
   assert response.status_code == 200
 
 def test_connect_wrongpass_ko():
@@ -27,7 +27,7 @@ def test_connect_wrongpass_ko():
   assert response.json() == {"detail": "Invalid credentials"}
 
 def test_connect_ok():
-  password = client.get("/users/sully_natsuya").json()['metas']['fpassword']
+  password = client.get("/user/sully_natsuya").json()['metas']['fpassword']
   response = client.post(
     "/connect/",
     headers={
