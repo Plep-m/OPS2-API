e2abe33ad4366b01a8ba2d1aa28e3acf25e99a38
Plep-m <you@example.com>
2023-12-11 19:47:33 +0100

    tests

--- tests/test_user_routes.py
+++ tests/test_user_routes.py
@@ -7,9 +7,17 @@ def test_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == ['Hello World !!!']
+
+def test_get_users_ok():
+  response = client.get("/users/")
+  assert response.status_code == 200
   
 def test_get_user_by_login_ok():
   response = client.get("/user/sully_natsuya")
+  assert response.status_code == 200
+
+def test_get_user_by_id_ok():
+  response = client.get("/user/1")
   assert response.status_code == 200
 
 def test_connect_wrongpass_ko():
