7fe797dc92619f1f362ab11a0fa1a0d94e7ec67f
Plep-m <menut34@gmail.com>
2023-12-12 11:37:24 +0100

    tests

---
+++ tests/test_user_utils.py
@@ -0,0 +1,10 @@
+from src.user_utils import create_user
+from src.database import SessionLocal
+from models.user_model import GenderEnum
+
+def test_create_user_ko():
+  with SessionLocal() as db:
+     try:
+      create_user(db, "TonyTony", "Chopper", GenderEnum.MALE, "+983456234", "45665", "456 Elm St", "Tokyo", "Japan", ["notarole"])
+     except Exception as e:
+      assert str(e) == "The specified roles were not found in the database."
