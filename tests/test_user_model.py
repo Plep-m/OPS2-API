02efc3c807f58144d2e56da66e4d71ba578a634e
Plep-m <menut34@gmail.com>
2023-12-12 14:06:20 +0100

    tests

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -2,8 +2,5 @@ from datetime import datetime, timedelta
 from models.user_model import User
 
 def test_age():
-    # Create a User instance with a birthday 20 years ago
     user = User(birthday=datetime.now() - timedelta(days=20*365))
-
-    # Check that the age property returns 20
-    assert user.age == 20
+    assert user.age == 19
