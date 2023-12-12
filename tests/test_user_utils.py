dfc6b5bd9b16a284cebabfd854f8d7d1c35cb3de
Plep-m <menut34@gmail.com>
2023-12-12 11:45:01 +0100

    tests

--- tests/test_user_utils.py
+++ tests/test_user_utils.py
@@ -8,3 +8,10 @@ def test_create_user_ko():
       create_user(db, "TonyTony", "Chopper", GenderEnum.MALE, "+983456234", "45665", "456 Elm St", "Tokyo", "Japan", ["notarole"])
      except Exception as e:
       assert str(e) == "The specified roles were not found in the database."
+
+def test_create_user_ko_2():
+  with SessionLocal() as db:
+    try:
+      create_user(db, "Nagisa", "Shiota", GenderEnum.NEUTRAL, "+983456734", "45665", "456 Elm St", "Tokyo", "Japan")
+    except Exception as e:
+      assert str(e) == "Nagisa Shiota already exist"
